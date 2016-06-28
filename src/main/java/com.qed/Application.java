63c18e22ad6fe31a6e496f156f66244eead623fc
liukaixuan <liukaixuan92@qq.com>
2016-06-28 16:44:16 +0800

    inti

---
+++ src/main/java/com.qed/Application.java
@@ -0,0 +1,17 @@
+package com.qed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class Application/* extends SpringBootServletInitializer*/ {
+
+    /*  @Override
+      protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+          return application.sources(Application.class);
+      }
+  */
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
